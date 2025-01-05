de4f0a6b681da97813565c294cb963fb0346900d
Simon Zwicker <github@zwicker.io>
2025-01-05 20:52:55 +0100

    Initial Commit

---
+++ MammutExample/MammutExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MammutExampleApp.swift
+//  MammutExample
+//
+//  Created by Simon Zwicker on 05.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MammutExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
